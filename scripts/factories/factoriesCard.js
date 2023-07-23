edd84127450b136dacd3d2a33f92059027443bac
Traore <traoreerick@gmail.com>
2023-07-23 21:11:32 +0200

    creation html css scss js bootstrap

--- scripts/factories/factoriesCard.js
+++ scripts/factories/factoriesCard.js
@@ -1,99 +1,129 @@
 function  cardFactory(data){
     const { image, name, servings, ingredients, time, description, appliance ,ustensils } = data;
+    let str = image
+    str = image.replaceAll ('.jpg', '');
+    let pict = str
+
     const picture = `assets/images/${image}`;
+    const newPicture = `assets/images/${pict}/destop.jpeg 1440w, assets/images/${pict}/mobile.jpeg 425w, assets/images/${pict}/tablette.jpeg 768w, assets/images/${pict}/tabletteXl.jpeg 1024w, assets/images/${pict}/ultra.jpeg 3000w`;
+    console.log(newPicture);
     const totalIngredients = ingredients.length;
     console.log(totalIngredients);
-
   
 function makeCardDOM () {
+
     const imghtml = document.createElement( 'img' );
     imghtml.setAttribute("src", picture)
+    imghtml.setAttribute("srcset", newPicture)
     imghtml.setAttribute("alt", name)
 
-    const cardhtml = document.createElement( 'card' );
-    cardhtml.classList.add ('main__displaycard__card');
-    cardhtml.appendChild(imghtml)
+    const card1html = document.createElement( 'div' );
+    card1html.classList.add ('card__all__recette__img');
 
-    const divhtml= document.createElement( 'div' );
-    divhtml.classList.add ('main__displaycard__card__title');
-    cardhtml.appendChild(divhtml)
+    const card0html = document.createElement( 'div' );
+    card0html.classList.add ('card__all__recette');
+    card0html.appendChild(card1html)
+    card1html.appendChild(imghtml)
+    
+    const card2html = document.createElement( 'div' );
+    card2html.classList.add ('card__all__recette__header');
+    card0html.appendChild(card2html)
 
-    const phtml= document.createElement( 'p' );
-    phtml.classList.add ('main__displaycard__card__title__p');
-    phtml.textContent = name;
-    divhtml.appendChild(phtml)
+    const card20html = document.createElement( 'div' );
+    card20html.classList.add ('card__all__recette__header__content');
+    card20html.textContent = data.time + 'min';
+    card2html.appendChild(card20html)
+   
+    const cardBody = document.createElement( 'div' );
+    cardBody.classList.add ('card__all__recette__body');
+    card0html.appendChild(cardBody)
 
-    const div2html= document.createElement( 'div' );
-    div2html.classList.add ('main__displaycard__card__recette');
-    cardhtml.appendChild(div2html)
+    const card3html = document.createElement( 'div' );
+    card3html.classList.add ('card__all__recette__body__content');
+    cardBody.appendChild(card3html)
 
-    const p2html= document.createElement( 'p' );
-    p2html.classList.add ('main__displaycard__card__recette__p');
-    p2html.textContent = "recette";
-    div2html.appendChild(p2html)
+    const card4html = document.createElement( 'div' );
+    card4html.classList.add ('card__all__recette__body__content__title');
+    card3html.appendChild(card4html)
+    
+    const card41html = document.createElement( 'P' );
+    card41html.classList.add ('card__all__recette__body__content__title__p');
+    card41html.textContent = data.name;
+    card4html.appendChild(card41html)
+  
+    const card5html = document.createElement( 'div' );
+    card5html.classList.add ('card__all__recette__body__content__recette');
+    card5html.textContent = "RECETTE";
+    card3html.appendChild(card5html)
 
-    const div3html= document.createElement( 'div' );
-    div3html.classList.add ('main__displaycard__card__descripton');
-    cardhtml.appendChild(div3html)
+    const card51html = document.createElement( 'div' );
+    card51html.classList.add ('card__all__recette__body__content__texte');
+    card3html.appendChild(card51html)
 
-    const p3html= document.createElement( 'p' );
-    p3html.classList.add ('main__displaycard__card__description__p');
-    p3html.textContent = description ;
-    div3html.appendChild(p3html)
+    const card6html = document.createElement( 'P' );
+    card6html.classList.add ('card__all__recette__body__content__texte__p');
+    card6html.textContent = data.description;
+    card51html.appendChild(card6html)
 
-    const div6html= document.createElement( 'div' );
-    div6html.classList.add ('main__displaycard__card__ingredient');
-    cardhtml.appendChild(div6html)
+    const card7html = document.createElement( 'div' );
+    card7html.classList.add ('card__all__recette__body__content__ingredients');
+    card3html.appendChild(card7html)
 
-    const p6html= document.createElement( 'p' );
-    p6html.classList.add ('main__displaycard__card__ingredient__p');
-    p6html.textContent = "ingrédient";
-    div6html.appendChild(p6html)
+    const card8html = document.createElement( 'p' );
+    card8html.classList.add ('card__all__recette__body__content__ingredients__p');
+    card8html.textContent = "Ingredients";
+    card7html.appendChild(card8html)
+
+    const card71html = document.createElement( 'div' );
+    card71html.classList.add ('card__all__recette__body__content__ingredients__items');
+    card3html.appendChild(card71html)
 
     creerDivIngredient()
-
-
     function creerDivIngredient(){
         ingredients.forEach((data) => {
-            // console.log(data);
+
+            const divxhtml= document.createElement( 'div' );
+            divxhtml.classList.add ('card__all__recette__body__content__ingredients__items__boucle');
+            card71html.appendChild(divxhtml)
+            
             const div6html= document.createElement( 'div' );
-            div6html.classList.add ('main__displaycard__card__ingredient__title');
-            cardhtml.appendChild(div6html)
+            div6html.classList.add ('card__all__recette__body__content__ingredients__items__boucle__title');
+            divxhtml.appendChild(div6html)
         
             const p6html= document.createElement( 'p' );
-            p6html.classList.add ('main__displaycard__card__ingredient__title__p');
+            p6html.classList.add ('card__all__recette__body__content__ingredients__items__boucle__title__p');
             p6html.textContent = data.ingredient;
             div6html.appendChild(p6html)
 
             const div7html= document.createElement( 'div' );
-            div7html.classList.add ('main__displaycard__card__ingredient__quantity');
-            cardhtml.appendChild(div7html)
+            div7html.classList.add ('card__all__recette__body__content__ingredients__items__boucle__quantity');
+            divxhtml.appendChild(div7html)
         
             const p7html= document.createElement( 'p' );
-            p7html.classList.add ('main__displaycard__card__ingredient__quantity__p');
+            p7html.classList.add ('card__all__recette__body__content__ingredients__items__boucle__quantity__p');
             p7html.textContent = data.quantity+" "+ data.unit;
             div7html.appendChild(p7html)
         })
     }
-    const div8html= document.createElement( 'div' );
-    div8html.classList.add ('main__displaycard__card__ingredient__time');
-    cardhtml.appendChild(div8html)
+    // const div8html= document.createElement( 'div' );
+    // div8html.classList.add ('card__recette__content__ingredient__time');
+    // card0html.appendChild(div8html)
 
-    const p8html= document.createElement( 'p' );
-    p8html.classList.add ('main__displaycard__card__ingredient__time__p');
-    p8html.textContent = data.time;
-    div8html.appendChild(p8html)
+    // const p8html= document.createElement( 'p' );
+    // p8html.classList.add ('card__recette__content__ingredient__time__p');
+    // p8html.textContent = data.time;
+    // div8html.appendChild(p8html)
 
-    const div9html= document.createElement( 'div' );
-    div9html.classList.add ('main__displaycard__card__ingredient__ustensils');
-    cardhtml.appendChild(div9html)
+    // const div9html= document.createElement( 'div' );
+    // div9html.classList.add ('card__recette__content__ingredient__ustensils');
+    // card0html.appendChild(div9html)
 
-    const p9html= document.createElement( 'p' );
-    p9html.classList.add ('main__displaycard__card__ingredient__ustensils__p');
-    p9html.textContent = data.ustensils;
-    div9html.appendChild(p9html)
+    // const p9html= document.createElement( 'p' );
+    // p9html.classList.add ('card__recette__content__ingredient__ustensils__p');
+    // p9html.textContent = data.ustensils;
+    // div9html.appendChild(p9html)
    
-    return (cardhtml);
+    return (card0html);
 }
 return { name, picture, makeCardDOM }
 
--- scripts/pages/index.js
+++ scripts/pages/index.js
@@ -1,24 +1,23 @@
 import Api from "../api/Api.js";
+
+let allRecipes = [];
  // Permet de récuperer toutes les données de l'api
         async function getItems() {
             const itemsApi = new Api('data/recipes.json')
             const items = await itemsApi.get()
+            allRecipes = items;
         return ({
             items })
     }
     // On affiche toutes les données (recettes') grace à l'utilisation du design patern factory
     async function  getDataCard(data) {
-      const cardSection = document.querySelector(".main__displaycard");
+      const cardSection = document.querySelector(".card__all");
       data.forEach((card) => {
         console.log('card',card);
         const cardModel = cardFactory(card);
         const makeCardDOM = cardModel.makeCardDOM();
         cardSection.appendChild(makeCardDOM);
-
-
-
       });
-
     }
     async function init() {
         // Récupère les datas des recipes
@@ -28,5 +27,10 @@ import Api from "../api/Api.js";
         getDataCard(dataCard)
       };
     init();
+    const inputSearch = document.getElementById('search');
+    inputSearch.addEventListener('change', (event) => {
+    console.log(event.target.value);
+    console.log(allRecipes);
+    });
 
 
