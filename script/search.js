a084b71d3f0545bcff82d171b3340a8ec04e45ca
Aqdas <aqdasdar91@gmail.com>
2024-02-12 13:07:05 +0100

    index et debut de la structure du projet

--- script/search.js
+++ script/search.js
@@ -1,35 +1,3 @@
-export function search(query) {
-}
-
-/*
-Début
-|
-|--> Initialiser results = []
-|
-|--> Pour chaque recette dans recipes :
-|     |
-|     |--> Si recette.title contient query OU recette.ingredients contient query OU recette.description contient query :
-|     |     |
-|     |     |--> Ajouter recette à results
-|     |
-|     |--> Fin Si
-|
-|--> Fin Pour
-|
-|--> Retourner results
-|
-Fin
-*/
-export function search(query, recipes) {
-    let results = [];
-    for (let i = 0; i < recipes.length; i++) {
-        if (recipes[i].title.includes(query) || recipes[i].ingredients.includes(query) || recipes[i].description.includes(query)) {
-            results.push(recipes[i]);
-        }
-    }
-    return results;
-}
-
 /*
 Début
 |
